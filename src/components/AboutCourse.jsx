9df70bdc5d61def56bf2c5505e5e9f3f1faaa9fb
Gustavo Farias <gustavogarcia266@hotmail.com>
2024-02-04 14:40:50 -0300

    AboutCourse background

--- src/components/AboutCourse.jsx
+++ src/components/AboutCourse.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./AboutCourse.css";
 
 const AboutCourse = () => {
-  return <div className="AboutCourse">AboutCourse</div>;
+  return <div className="AboutCourse"></div>;
 };
 
 export default AboutCourse;
